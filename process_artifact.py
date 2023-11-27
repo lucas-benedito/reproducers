e3709797bdf3d9cd8b0773679361b392c5f91a1a
Lucas Benedito <lbenedit@redhat.com>
2023-11-27 20:19:42 +0000

    First upload

---
+++ process_artifact.py
@@ -0,0 +1,25 @@
+import json
+import os
+import glob
+
+# Read the path where the json reside
+artifact_discovery = input("Path for the artifacts: ")
+path_to_search = os.path.abspath(artifact_discovery)
+# search all json files
+search_dir = os.path.join(path_to_search, "*.json")
+
+json_files = glob.glob(search_dir, recursive=False)
+
+# Create an empty list to store the Python objects.
+python_objects = []
+
+# Load each JSON file into a Python object.
+for json_file in json_files:
+    with open(json_file, "r") as f:
+        python_objects.append(json.load(f))
+
+lines = sorted(python_objects, key=lambda k: k['created'], reverse=False)
+
+# Dump all the Python objects into a single JSON file.
+with open("/tmp/combined.json", "w") as f:
+    json.dump(lines, f, indent=4)
