00167e60374b85dde13758f3b7d48d076dab7a7f
Lucas Benedito <lbenedit@redhat.com>
2023-11-27 20:26:59 +0000

    Added README and user info

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# reproducers
+# reproducers - artifact_processing
+
+```
+$ python process_artifact.py
+Path for the artifacts: artifacts/99999/job_events
+Files combined into /tmp/combined.json
+$ head  /tmp/combined.json
+[
+    {
+        "event": "playbook_on_task_start",
+        "job_id": 99999,
+        "pid": 99,
+```
--- process_artifact.py
+++ process_artifact.py
@@ -10,16 +10,21 @@ search_dir = os.path.join(path_to_search, "*.json")
 
 json_files = glob.glob(search_dir, recursive=False)
 
-# Create an empty list to store the Python objects.
-python_objects = []
+if json_files:
+    # Create an empty list to store the Python objects.
+    python_objects = []
 
-# Load each JSON file into a Python object.
-for json_file in json_files:
-    with open(json_file, "r") as f:
-        python_objects.append(json.load(f))
+    # Load each JSON file into a Python object.
+    for json_file in json_files:
+        with open(json_file, "r") as f:
+            python_objects.append(json.load(f))
 
-lines = sorted(python_objects, key=lambda k: k['created'], reverse=False)
+    lines = sorted(python_objects, key=lambda k: k['created'], reverse=False)
 
-# Dump all the Python objects into a single JSON file.
-with open("/tmp/combined.json", "w") as f:
-    json.dump(lines, f, indent=4)
+    # Dump all the Python objects into a single JSON file.
+    with open("/tmp/combined.json", "w") as f:
+        json.dump(lines, f, indent=4)
+
+    print("Files combined into /tmp/combined.json")
+else:
+    print("No JSON files found to process.")
